ad7b6beadd79f842768e45bd9bf258fbf0a252e5
Max <meitstudying@gmail.com>
2024-12-04 23:31:00 +0200

    Implement unit tests and CI integration

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     FuncA calc;
-    double x = 1.0;
-    int n = 10; 
+    double x = 1.0; 
+    int n = 10;
     std::cout << "FuncA result: " << calc.calculate(x, n) << std::endl;
     return 0;
 }
